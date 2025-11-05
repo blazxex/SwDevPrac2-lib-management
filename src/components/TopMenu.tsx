fd15fc4aedb96dc72e194407ce2923f461d75992
Blazxex <armygm162@gmail.com>
2025-11-05 16:09:58 +0700

    feat: redirect to main

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,34 +1,41 @@
 import Image from "next/image";
 import TopMenuItem from "./TopMenuItem";
-import { getServerSession } from 'next-auth'
+import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default async function TopMenu() {
+  const session = await getServerSession(authOptions);
 
-    const session = await getServerSession(authOptions)
-
-    return (
-        <div className="h-[50px] bg-orange-100 fixed top-0 left-0 right-0 z-30 flex flex-row justify-end">
-            
-            <div className='flex flex-row absolute left-0 h-full'>
-                {
-                    session? 
-                        <Link href="/api/auth/signout">
-                            <div className='flex items-center h-full px-5 text-yellow-900 font-bold font-sans underline'>
-                                Sign-Out of {session.user?.name}
-                            </div>
-                        </Link>
-                        :<Link href="/api/auth/signin">
-                            <div className='flex items-center h-full px-5 text-yellow-900 font-bold font-sans underline'>
-                                Sign-In
-                            </div>
-                        </Link>
-                }
-                <TopMenuItem title="Reservations" pageRef="/reservations" />
+  return (
+    <div className="h-[50px] bg-orange-100 fixed top-0 left-0 right-0 z-30 flex flex-row justify-end">
+      <div className="flex flex-row absolute left-0 h-full">
+        {session ? (
+          <Link href="/api/auth/signout">
+            <div className="flex items-center h-full px-5 text-yellow-900 font-bold font-sans underline">
+              Sign-Out of {session.user?.name}
             </div>
-            <TopMenuItem title="Reserve Book" pageRef="/reserve" />
-            <Image src={"/img/logo.png"} className="h-full w-auto" alt="logo" width={0} height={0} sizes="100vh" />
-        </div>
-    );
+          </Link>
+        ) : (
+          <Link href="/api/auth/signin">
+            <div className="flex items-center h-full px-5 text-yellow-900 font-bold font-sans underline">
+              Sign-In
+            </div>
+          </Link>
+        )}
+        <TopMenuItem title="Reservations" pageRef="/reservations" />
+      </div>
+      <TopMenuItem title="Reserve Book" pageRef="/reserve" />
+      <Link href="/">
+        <Image
+          src={"/img/logo.png"}
+          className="h-full w-auto"
+          alt="logo"
+          width={0}
+          height={0}
+          sizes="100vh"
+        />
+      </Link>
+    </div>
+  );
 }
